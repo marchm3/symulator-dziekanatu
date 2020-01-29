00eae30872d6e4729d78642f69a13a19a25b9a0c
Marcin Chmielowiec <marchm3@st.amu.edu.pl>
2020-01-29 13:55:19 +0100

    interfejs restowy

--- src/main/java/Simulation/Application.java
+++ src/main/java/Simulation/Application.java
@@ -1,13 +1,13 @@
-package com.example.SymulatorDziekanatu;
+package Simulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SymulatorDziekanatuApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SymulatorDziekanatuApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/Simulation/SimulationController.java
+++ src/main/java/Simulation/SimulationController.java
@@ -1,13 +1,28 @@
 package Simulation;
 
+import com.example.SymulatorDziekanatu.Report;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class SimulationController {
+    @Autowired
+    Simulation simulation;
 
-    @PostMapping
-    public void startNew() {
 
+    @PostMapping("/start")
+    public void startNew(@RequestBody SimulationConfig config) {
+
+    }
+
+    @PostMapping("/process")
+    public void startNew(@RequestParam Integer number) {
+
+    }
+
+    @GetMapping("/report")
+    public Report getReport() {
+        return simulation.getReport();
     }
 }
